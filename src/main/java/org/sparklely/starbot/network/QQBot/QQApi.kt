dc971700ae3aae5d3c385d2f39652c0cba8b50ab
FengXiang2233 <fjx13125@163.com>
2024-07-12 21:39:40 +0800

    实现部分QQAPI

--- src/main/java/org/sparklely/starbot/network/QQBot/QQApi.kt
+++ src/main/java/org/sparklely/starbot/network/QQBot/QQApi.kt
@@ -8,22 +8,42 @@ import org.sparklely.starbot.network.Request
 
 class QQApi {
 
+    private val mapType: Type = object : TypeToken<Map<String, Any>>() {}.type
+    private val apiAddress="https://api.sgroup.qq.com"
+
     private var appId:String = ""
     private var clientSecret:String = ""
-    private var appAccessToken:MutableMap<String,String> =  mutableMapOf()
+    private var appAccessToken:MutableMap<String,Any> =  mutableMapOf()
+    private var requestHeaders:HashMap<String,String> = hashMapOf()
 
     fun login(appId:String,clientSecret:String){
         this.appId=appId
         this.clientSecret=clientSecret
+        getAppAccessToken()
     }
 
-    fun getAppAccessToken(){
+    private fun getAppAccessToken(){
         val data= hashMapOf<String,Any>()
-        data["appId"]=this.appId
-        data["clientSecret"]=this.clientSecret
-        val mapType: Type = object : TypeToken<Map<String, Any>>() {}.type
-        this.appAccessToken = Gson().fromJson(Request().post("https://bots.qq.com/app/getAppAccessToken",
-                        data = data).body(), mapType)
-        this.appAccessToken["expires_time"]=""
+        data["appId"]=appId
+        data["clientSecret"]=clientSecret
+        appAccessToken = Gson().fromJson(Request().post("https://bots.qq.com/app/getAppAccessToken", data = data).body(), mapType)
+        appAccessToken["expires_time"]=System.currentTimeMillis()+Integer.parseInt(appAccessToken["expires_in"] as String)
+        appAccessToken.remove("expires_in")
+        requestHeaders["Authorization"]="QQBot ${appAccessToken["access_token"]}"
+        requestHeaders["X-Union-Appid"]=appId
+        requestHeaders["Content-Type"]="application/json"
     }
+
+    fun checkAppAccessToken(){
+        if(Integer.parseInt(appAccessToken["expires_in"] as String)-System.currentTimeMillis()<=60)
+            getAppAccessToken()
+    }
+
+    private fun getWSAddress(): String?{
+        var data = hashMapOf<String,String>()
+        data = Gson().fromJson(Request().get("$apiAddress/gateway", header = requestHeaders).body(), mapType)
+        return data["url"]
+    }
+
 }
+
--- src/main/java/org/sparklely/starbot/network/Request.kt
+++ src/main/java/org/sparklely/starbot/network/Request.kt
@@ -5,7 +5,6 @@ import java.net.URI;
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
-import java.util.Objects
 
 class Request {
 
