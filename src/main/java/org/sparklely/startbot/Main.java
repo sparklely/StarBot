5f8787a2a7f48ff22b851c517516d6c37de67a78
CSneko <minecrafthetun@outlook.com>
2024-07-12 12:43:30 +0800

    init

---
+++ src/main/java/org/sparklely/startbot/Main.java
@@ -0,0 +1,7 @@
+package org.sparklely.startbot;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/sparklely/startbot/test.kt
@@ -0,0 +1,4 @@
+package org.sparklely.startbot
+
+class test {
+}
